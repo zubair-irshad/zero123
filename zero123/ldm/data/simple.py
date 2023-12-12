91960dd908e7cf57d658b0ec2fdd2c440bf69892
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-12-12 00:24:13 -0500

    add partnet mobility finetuning

--- zero123/ldm/data/simple.py
+++ zero123/ldm/data/simple.py
@@ -284,7 +284,7 @@ class ObjaverseData(Dataset):
         self.tform = image_transforms
 
     def __len__(self):
-        return len(self.paths)
+        return 100*len(self.paths)
         
     def cartesian_to_spherical(self, xyz):
         ptsnew = np.hstack((xyz, np.zeros(xyz.shape)))
